faf4349a84a2b705a42750d3f578dea2ffca2fd7
hp <hp@DESKTOP-FV0GDEQ>
2026-04-02 19:05:27 +0530

    This is second commit

--- PracJava/src/practice/FirstProg.java
+++ PracJava/src/practice/FirstProg.java
@@ -6,6 +6,15 @@ public class FirstProg {
 		
 		String car="Ertiga";
 		System.out.println(car);
+		
+		String cars[]= {"Sumo","Safari","Santro","Creata","Civic"};
+		
+		for(int i=0;i<=cars.length-1;i++) {
+			if(cars[i].startsWith("S")) {
+				System.out.println(cars[i]);
+			}
+		}
+		
 
 	}
 
