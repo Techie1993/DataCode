3d88785edbdc1b779e8c873af4fc9aa85756f018
hp <hp@DESKTOP-FV0GDEQ>
2026-04-02 19:20:23 +0530

    Square Digits...

---
+++ PracJava/src/practice/ArrayOps.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class ArrayOps {
+
+	public static void main(String[] args) {
+		
+
+		int data[]= {11,22,13};
+		
+		for(int i=0;i<=data.length-1;i++) {
+			int square=data[i] * data[i];
+			System.out.println(square);
+		}
+	
+		
+		
+	}
+}
