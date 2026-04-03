46580ded8f46cee892d1e44efcd5e7ae6a59efac
hp <hp@DESKTOP-FV0GDEQ>
2026-04-03 12:17:30 +0530

    Company Filter,Sixth Commit

---
+++ PracJava/src/practice/Companies.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class Companies {
+
+	public static void main(String[] args) {
+		
+		String comp[]= {"HCL","Cybage","Delloite","Capgemini","Cognizant"};
+		
+		for(int i=0;i<=comp.length-1;i++) {
+			if(comp[i].startsWith("C")) {
+				System.out.println(comp[i]);
+			}
+		}
+		
+				
+		
+		
+
+	}
+}
